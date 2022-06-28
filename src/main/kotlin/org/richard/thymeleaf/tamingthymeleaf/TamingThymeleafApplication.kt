1784b51173b147f3c1e21fc0600df8a78fded6d3
Richard Agyei <richard.agyei@gmail.com>
2022-06-27 23:40:21 -0500

    Initial

---
+++ src/main/kotlin/org/richard/thymeleaf/tamingthymeleaf/TamingThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package org.richard.thymeleaf.tamingthymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TamingThymeleafApplication
+
+fun main(args: Array<String>) {
+	runApplication<TamingThymeleafApplication>(*args)
+}
